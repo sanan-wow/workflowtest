e5c3d3a9ce314e55d0c010840f424cd56ebf5771
Sanan Ibrahimov <sibrahimov@woolworths.com.au>
2021-09-25 22:05:32 +1000

    testing  the workflow

--- beautifulsoap.py
+++ beautifulsoap.py
@@ -10,5 +10,14 @@ results = soup.find(id="ResultsContainer")
 
 job_elements = results.find_all("div", class_="card-content")
 
+# for job_element in job_elements:
+#     print(job_element, end="\n"*2)
+
 for job_element in job_elements:
-    print(job_element, end="\n"*2)
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text)
+    print(company_element.text)
+    print(location_element.text)
+    print()
